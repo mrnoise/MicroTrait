3d53199ed3c934bd82a7f87b813f50bf0f179fc4
Steffen Fuchs <fuchs@kobold.com>
2020-10-12 10:43:01 +0200

    added version scheme

---
+++ Version.hpp
@@ -0,0 +1,10 @@
+
+#ifndef MICROTRAIT_VERSION_HPP_
+#define MICROTRAIT_VERSION_HPP_
+
+#define MT_VER_MAJOR 1
+#define MT_VER_MINOR 0
+#define MT_VER_PATCH 0
+#define MT_VER_BUILD 0
+
+#endif /* MICROTRAIT_VERSION_HPP_ */
