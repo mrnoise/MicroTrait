eb3aa784e8623c4695d31eb474c358cfdce629fc
MrNoise <steffenfuchs@me.com>
2020-10-15 12:58:36 +0200

    added option to activate MSP430 lib

--- Settings.hpp
+++ Settings.hpp
@@ -2,6 +2,6 @@
 #define MICROTRAIT_SETTINGS_HPP_
 
 
-//#define MT_USE_MSP430_LIB /* if commented in the MSP430 lib is activated  */
+#define MT_USE_MSP430_LIB /* if commented in the MSP430 lib is activated  */
 
 #endif /* MICROTRAIT_SETTINGS_HPP_ */
