3919a139be65e2c1c7543209118cfbf24743d46d
MrNoise <steffenfuchs@me.com>
2020-10-16 10:12:33 +0200

    added doxypress documentation

---
+++ Docs/navtreedata.js
@@ -0,0 +1,141 @@
+var NAVTREE =
+[
+  [ "MicroTrait C++ Microcontroller Library", "index.html", [
+    [ "MicroTrait", "md_README.html", null ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", [
+        [ "MT", null, [
+          [ "MSP430", null, [
+            [ "GPIO", null, [
+              [ "Internal", null, [
+                [ "Port", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1Port.html", [
+                  [ "getInputPinValue", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1Port.html#a1ec54e4da04a7583ccaad25b452acfed", null ],
+                  [ "setAsInputPin", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1Port.html#a070d29fda1ba25c38cda1b5c55065481", null ],
+                  [ "setAsInputPinWithPullDown", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1Port.html#a54c8e7aebe19e0f4021e1088dd9d278f", null ],
+                  [ "setAsInputPinWithPullUp", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1Port.html#adf4bfa21ec760831438f84fe7baeb8c6", null ],
+                  [ "setAsOutputPin", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1Port.html#a242d721bccbb96127127ef6236cb6be2", null ],
+                  [ "setAsPeripheralModuleFunctionInputPin", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1Port.html#a0d257e77bfce325fcb0cbc422d595d2d", null ],
+                  [ "setAsPeripheralModuleFunctionOutputPin", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1Port.html#ad8d6b1d440f01e5267f3d992260da81c", null ],
+                  [ "setOutputHighOnPin", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1Port.html#ab50d14a484802eaf3eb65b11416aac40", null ],
+                  [ "setOutputLowOnPin", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1Port.html#af11e9438b419cf6175b164fb19411cf1", null ],
+                  [ "toggleOutputOnPin", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1Port.html#a87e1f68c5bd6739ef54a6d79d85e2399", null ]
+                ] ],
+                [ "PortWithInt", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1PortWithInt.html", [
+                  [ "clearInterrupt", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1PortWithInt.html#a2b3f70914035d012482dd7868964970d", null ],
+                  [ "disableInterrupt", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1PortWithInt.html#a6081ec1d4826266878537d6f17002aa1", null ],
+                  [ "enableInterrupt", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1PortWithInt.html#a7ff497469dcecb1543dfc8dea9c6d7d8", null ],
+                  [ "getInputPinValue", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1PortWithInt.html#a39d7dfca3d951783a99a324d2db5ce98", null ],
+                  [ "getInterruptStatus", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1PortWithInt.html#a45c46b5428d63ce14a8c77b70f3e23eb", null ],
+                  [ "selectInterruptEdge", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1PortWithInt.html#a1aede0078274977a43d8e07d751e1fd9", null ],
+                  [ "setAsInputPin", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1PortWithInt.html#ae655a13760c53990590dcac9a944206e", null ],
+                  [ "setAsInputPinWithPullDown", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1PortWithInt.html#ae0ddd1eaf216b18631a6174a0c7f2cd7", null ],
+                  [ "setAsInputPinWithPullUp", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1PortWithInt.html#ab66cf5451af99e396af89d9d0f4bcfea", null ],
+                  [ "setAsOutputPin", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1PortWithInt.html#ad911a5577b2ba230666bbb297cd5544d", null ],
+                  [ "setAsPeripheralModuleFunctionInputPin", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1PortWithInt.html#a7b8fbe4c41dedb70255176f4299023fa", null ],
+                  [ "setAsPeripheralModuleFunctionOutputPin", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1PortWithInt.html#aca072f1523fb6e544ba068f07365ec0b", null ],
+                  [ "setOutputHighOnPin", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1PortWithInt.html#a0bdd306ce172b848dd692b3b3bf9fda7", null ],
+                  [ "setOutputLowOnPin", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1PortWithInt.html#a9526906127c157372709443b8a2e8458", null ],
+                  [ "toggleOutputOnPin", "structMT_1_1MSP430_1_1GPIO_1_1Internal_1_1PortWithInt.html#af673f9ff7ead9b73a0cb9671552a974d", null ]
+                ] ]
+              ] ]
+            ] ],
+            [ "WDT", null, [
+              [ "WdtA", "structMT_1_1MSP430_1_1WDT_1_1WdtA.html", [
+                [ "hold", "structMT_1_1MSP430_1_1WDT_1_1WdtA.html#a6f36dd33bf716e239d5f7154a841de36", null ],
+                [ "initIntervalTimer", "structMT_1_1MSP430_1_1WDT_1_1WdtA.html#abec2c96381295e32fb59d8cc06dd3bb7", null ],
+                [ "initWatchdogTimer", "structMT_1_1MSP430_1_1WDT_1_1WdtA.html#a4f77bbc462843ff69ba554b9dc802a57", null ],
+                [ "resetTimer", "structMT_1_1MSP430_1_1WDT_1_1WdtA.html#a034a1e0ed53427d6f198d56d3daceab0", null ],
+                [ "start", "structMT_1_1MSP430_1_1WDT_1_1WdtA.html#affb864b16f1da5b994ba4b884b02cb92", null ],
+                [ "startIntervalTimer", "structMT_1_1MSP430_1_1WDT_1_1WdtA.html#a4068f41b723dcffb681f409768b6e5ba", null ],
+                [ "startWatchdogTimer", "structMT_1_1MSP430_1_1WDT_1_1WdtA.html#abb1ea835be4d7339d66648391ebf4f3f", null ]
+              ] ]
+            ] ],
+            [ "Sfr", "structMT_1_1MSP430_1_1Sfr.html", [
+              [ "NMI_EDGE", "structMT_1_1MSP430_1_1Sfr.html#a97b1da589340085a5cffe44121ba87e5", null ],
+              [ "RST_FUNC", "structMT_1_1MSP430_1_1Sfr.html#af5ba968e328238eed9003c8b2331757e", null ],
+              [ "RST_RESISTOR", "structMT_1_1MSP430_1_1Sfr.html#ad4f650715a2401a65096d4455016994e", null ],
+              [ "clearInterruptStatus", "structMT_1_1MSP430_1_1Sfr.html#af628c7d2a7fabf782e8cf41131d8447d", null ],
+              [ "disableInterrupt", "structMT_1_1MSP430_1_1Sfr.html#a6aa4ac0a33b9ea50314022823249584c", null ],
+              [ "enableInterrupt", "structMT_1_1MSP430_1_1Sfr.html#a49cf87f702a24bc0ac1ffe8639b02821", null ],
+              [ "getInterruptStatus", "structMT_1_1MSP430_1_1Sfr.html#a6dcf90c755e55f5075ee1d3b0fc2d306", null ],
+              [ "setNMIEdge", "structMT_1_1MSP430_1_1Sfr.html#a6717cd4f678cce1bdc6e505331dcaa53", null ],
+              [ "setResetNMIPinFunction", "structMT_1_1MSP430_1_1Sfr.html#abce2ac33cdfa02bc215ab27693d7f8f8", null ],
+              [ "setResetPinPullResistor", "structMT_1_1MSP430_1_1Sfr.html#a095995edb9190d9c557506dafe3ad399", null ]
+            ] ]
+          ] ],
+          [ "Universal", null, [
+            [ "Interrupt", null, [
+              [ "Interrupt", "structMT_1_1Universal_1_1Interrupt_1_1Interrupt.html", [
+                [ "Interrupt", "structMT_1_1Universal_1_1Interrupt_1_1Interrupt.html#a9c5df031de011c4df309d61d047858af", null ],
+                [ "get_index", "structMT_1_1Universal_1_1Interrupt_1_1Interrupt.html#a6e82c0a4495ea0048d508cde8001b4ba", null ],
+                [ "m_indexes", "structMT_1_1Universal_1_1Interrupt_1_1Interrupt.html#a3cef9f57971a9e765cc1c838cadfbb19", null ],
+                [ "m_vectors", "structMT_1_1Universal_1_1Interrupt_1_1Interrupt.html#a712c90866329e99eab5a09a7abe0a6d1", null ]
+              ] ]
+            ] ],
+            [ "Register", "structMT_1_1Universal_1_1Register.html", [
+              [ "clear", "structMT_1_1Universal_1_1Register.html#afae65432e8984b3cfcc03ee2e758f5f2", null ],
+              [ "clear", "structMT_1_1Universal_1_1Register.html#a1f428c01b6d733779634da722ec91db9", null ],
+              [ "compare", "structMT_1_1Universal_1_1Register.html#a8fba4c59f983601c5770a4a633bc0f3f", null ],
+              [ "compare", "structMT_1_1Universal_1_1Register.html#ae6f6bfed0e473fa345d1403c88e93b2d", null ],
+              [ "get", "structMT_1_1Universal_1_1Register.html#ac0b2d175099449e40ff542a1d4e784c3", null ],
+              [ "override", "structMT_1_1Universal_1_1Register.html#a24b4742136f0eb2c7592d68d2bf9b901", null ],
+              [ "override", "structMT_1_1Universal_1_1Register.html#a97f08734dd527439f4741af51c6f7342", null ],
+              [ "set", "structMT_1_1Universal_1_1Register.html#ab869ad638e9f151cf6127174ee71ee35", null ],
+              [ "set", "structMT_1_1Universal_1_1Register.html#a2f7b02a463758ea708dc6b0a6a4187cf", null ],
+              [ "toggle", "structMT_1_1Universal_1_1Register.html#a21234920d6d580d8eec6f37c4c7bfd24", null ],
+              [ "toggle", "structMT_1_1Universal_1_1Register.html#ab42a758aa2548fb9abba996087661e99", null ]
+            ] ]
+          ] ]
+        ] ],
+        [ "enable_Enum_bits", "structenable__Enum__bits.html", null ]
+      ] ],
+      [ "Class Index", "classes.html", null ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", [
+        [ "MSP430", "dir_56021fdb0e8b9a80f75a9a8cba132c6e.html", [
+          [ "GPIO", "dir_406c66aff740586c12c555eb45f89962.html", null ],
+          [ "SFR", "dir_eb46cb3a04801aaa57212b6bb39afb1a.html", null ],
+          [ "WDT", "dir_ccb66b0757fb9c81f3459334107c554a.html", null ]
+        ] ],
+        [ "Misc", "dir_b11b26c345ed0f7c972184e81ad6f440.html", null ],
+        [ "Universal", "dir_8a747bb3c4d720dc480715835e2d305c.html", null ]
+      ] ]
+    ] ],
+    [ "File Source", "filesource.html", [
+      [ "MSP430", "dir_56021fdb0e8b9a80f75a9a8cba132c6e.html", [
+        [ "GPIO", "dir_406c66aff740586c12c555eb45f89962.html", [
+          [ "PortInterrupt.hpp", "PortInterrupt_8hpp_source.html", null ],
+          [ "PortRegister.hpp", "PortRegister_8hpp_source.html", null ]
+        ] ],
+        [ "SFR", "dir_eb46cb3a04801aaa57212b6bb39afb1a.html", [
+          [ "Sfr.hpp", "Sfr_8hpp_source.html", null ]
+        ] ],
+        [ "WDT", "dir_ccb66b0757fb9c81f3459334107c554a.html", [
+          [ "WdtA.hpp", "WdtA_8hpp_source.html", null ],
+          [ "WdtAInterrupt.hpp", "WdtAInterrupt_8hpp_source.html", null ]
+        ] ],
+        [ "Settings.hpp", "MSP430_2Settings_8hpp_source.html", null ]
+      ] ],
+      [ "Misc", "dir_b11b26c345ed0f7c972184e81ad6f440.html", [
+        [ "Details.hpp", "Details_8hpp_source.html", null ],
+        [ "EnumBits.hpp", "EnumBits_8hpp_source.html", null ]
+      ] ],
+      [ "Universal", "dir_8a747bb3c4d720dc480715835e2d305c.html", [
+        [ "Interrupt.hpp", "Interrupt_8hpp_source.html", null ],
+        [ "Register.hpp", "Register_8hpp_source.html", null ]
+      ] ],
+      [ "MT.hpp", "MT_8hpp_source.html", null ],
+      [ "Settings.hpp", "Settings_8hpp_source.html", null ],
+      [ "Version.hpp", "Version_8hpp_source.html", null ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+".html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronization';
+var SYNCOFFMSG = 'click to enable panel synchronization';
