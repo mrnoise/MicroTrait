b623802c0b38ec970291b143bb2b6b3e9faf6b5e
MrNoise <steffenfuchs@me.com>
2020-10-22 15:09:44 +0200

    added docs for Settings.hpp

--- MSP430/Settings.hpp
+++ MSP430/Settings.hpp
@@ -1,3 +1,5 @@
+/** @file */
+
 /** @defgroup groupMSP430 MSP430
 * MSP430 specific code
 */
@@ -9,6 +11,14 @@
 * @details
 * Usage: \code {.cpp}
 
+//if commented in the lib will be work internally like the driverlib / if commented out the lib will remove needless code
+#define MT_MSP430_USE_DRIVERLIB_COMPATIBILITY
+
+//if commented in the lib will register GPIO Interrupts with callbacks known at compile time -> for max performance
+#define MT_MSP430_USE_GPIO_COMPILE_TIME_CALLBACKS
+
+//if commented in the lib will register WDT Interrupts with callbacks known at compile time -> for max performance
+#define MT_MSP430_USE_WDT_COMPILE_TIME_CALLBACKS
 
 \endcode
 *
@@ -18,16 +28,20 @@
 *
 ****************************************************************
 */
+
 #ifndef MICROTRAIT_MSP430_SETTINGS_HPP_
 #define MICROTRAIT_MSP430_SETTINGS_HPP_
 
 #include "MicroTrait/Settings.hpp"
 
-//#define MT_MSP430_USE_DRIVERLIB_COMPATIBILITY /* if commented in the lib will be work internally like the driverlib / if commented out the lib will remove needless code  */
+/*if commented in the lib will be work internally like the driverlib / if commented out the lib will remove needless code*/
+//#define MT_MSP430_USE_DRIVERLIB_COMPATIBILITY
 
-#define MT_MSP430_USE_GPIO_COMPILE_TIME_CALLBACKS /* if commented in the lib will register GPIO Interrupts with callbacks known at compile time - >for max performance  */
+/*if commented in the lib will register GPIO Interrupts with callbacks known at compile time -> for max performance*/
+#define MT_MSP430_USE_GPIO_COMPILE_TIME_CALLBACKS
 
-#define MT_MSP430_USE_WDT_COMPILE_TIME_CALLBACKS /* if commented in the lib will register WDT Interrupts with callbacks known at compile time - >for max performance  */
+/*if commented in the lib will register WDT Interrupts with callbacks known at compile time -> for max performance*/
+#define MT_MSP430_USE_WDT_COMPILE_TIME_CALLBACKS
 
 
 #ifndef MT_MSP430_USE_DRIVERLIB_COMPATIBILITY
